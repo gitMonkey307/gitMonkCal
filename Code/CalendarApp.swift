22ae370db1205a08a027ce673797ac5c154efeab
gitMonkey307 <monkgamingyt@gmail.com>
2026-04-05 01:26:34 -0500

    Update CalendarApp to use ContentView
    Replaced MonthView with ContentView in CalendarApp.

--- Code/CalendarApp.swift
+++ Code/CalendarApp.swift
@@ -2,15 +2,9 @@ import SwiftUI
 
 @main
 struct CalendarApp: App {
-    @StateObject private var viewModel = CalendarViewModel()
-
     var body: some Scene {
         WindowGroup {
-            MonthView(groupedEvents: viewModel.groupedEvents)
-                .task {
-                    // Ask for permission and load events the second the app opens
-                    await viewModel.requestAccessAndFetch()
-                }
+            ContentView()
         }
     }
 }
