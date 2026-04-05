fcb54d4ed4b2e538d3a4a900663dcffaf40d1f98
gitMonkey307 <monkgamingyt@gmail.com>
2026-04-05 00:58:39 -0500

    Add files via upload

--- Code/CalendarApp.swift
+++ Code/CalendarApp.swift
@@ -3,9 +3,14 @@ import SwiftUI
 @main
 struct CalendarApp: App {
     @StateObject private var viewModel = CalendarViewModel()
+
     var body: some Scene {
         WindowGroup {
-            MainView(viewModel: viewModel)
+            MonthView(groupedEvents: viewModel.groupedEvents)
+                .task {
+                    // Ask for permission and load events the second the app opens
+                    await viewModel.requestAccessAndFetch()
+                }
         }
     }
 }
