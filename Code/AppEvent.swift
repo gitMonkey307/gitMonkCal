4bc67c5f26c36695b04881ccb501ad82dedc13b3
gitMonkey307 <monkgamingyt@gmail.com>
2026-04-05 00:46:15 -0500

    Add calendarID to AppEvent struct
    Added calendarID property to AppEvent struct.

--- Code/AppEvent.swift
+++ Code/AppEvent.swift
@@ -1,5 +1,4 @@
 import SwiftUI
-import EventKit
 
 public struct AppEvent: Identifiable, Hashable {
     public let id: String
@@ -11,22 +10,8 @@ public struct AppEvent: Identifiable, Hashable {
     public var notes: String?
     public var colorHex: String
     public var isTask: Bool
-    public var isCompleted: Bool = false // Only for tasks
+    public var isCompleted: Bool = false
+    public var calendarID: String
     
-    public var displayColor: Color {
-        Color(hex: colorHex) ?? .blue
-    }
-}
-
-// Extension to handle Hex colors natively
-extension Color {
-    init?(hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-        var rgb: UInt64 = 0
-        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
-        self.init(red: Double((rgb & 0xFF0000) >> 16) / 255.0,
-                  green: Double((rgb & 0x00FF00) >> 8) / 255.0,
-                  blue: Double(rgb & 0x0000FF) / 255.0)
-    }
+    public var displayColor: Color { Color(hex: colorHex) ?? .blue }
 }
