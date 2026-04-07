0a35dfc46ab3fda3cc7409ca2a96e2dc2eb42b72
gitMonkey307 <monkgamingyt@gmail.com>
2026-04-07 05:28:37 -0500

    Update CalendarApp.swift

--- Code/CalendarApp.swift
+++ Code/CalendarApp.swift
@@ -3,16 +3,15 @@ import SwiftUI
 @main
 struct CalendarApp: App {
     @StateObject private var viewModel = CalendarViewModel()
-    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(viewModel)
-        }
-        // iOS 17 Native two-parameter implementation
-        .onChange(of: scenePhase) { oldPhase, newPhase in
-            if newPhase == .active { Task { await viewModel.refreshData() } }
+                .onOpenURL { url in
+                    // Logic from gitMonk Interactive: Hand-off from Widget
+                    viewModel.handleDeepLink(url: url)
+                }
         }
     }
 }
